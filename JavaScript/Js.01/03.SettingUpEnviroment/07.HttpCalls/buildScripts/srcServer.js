e509c9b5b1d5cda0ff7e10659bb2ef9da4dc412c
SageGandhi <prajit.ju@gmail.com>
2018-05-09 01:17:04 +0530

    HttpCall Done

--- JavaScript/Js.01/03.SettingUpEnviroment/07.HttpCalls/buildScripts/srcServer.js
+++ JavaScript/Js.01/03.SettingUpEnviroment/07.HttpCalls/buildScripts/srcServer.js
@@ -10,4 +10,5 @@ const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+app.get('/users', (req, res) => res.json([{ "id": 247119, "firstName": "Prajit", "lastName": "Gandhi", "email": "Prajit.Gandhi@gmail.com" }]));
 app.listen(port, (err) => err ? console.log(err) : open('http://localhost:' + port));//eslint-disable-line no-console
