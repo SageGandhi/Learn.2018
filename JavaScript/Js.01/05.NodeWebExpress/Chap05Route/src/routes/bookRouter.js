e5553a7f07708f91a7019a60cc4d5c4c23075c2e
SageGandhi <prajit.ju@gmail.com>
2018-05-17 07:08:47 +0530

    NodeJs Express Books With Darth Vader

---
+++ JavaScript/Js.01/05.NodeWebExpress/Chap05Route/src/routes/bookRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+// Wrap Inside A Function To Pass Config Fo Navigation Object
+const bookRouter = express.Router();
+function router(navigation) {
+  const books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    },
+    {
+      title: 'Les Misérables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false
+    },
+    {
+      title: 'The Time Machine',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false
+    },
+    {
+      title: 'A Journey into the Center of the Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false
+    },
+    {
+      title: 'The Dark World',
+      genre: 'Fantasy',
+      author: 'Henry Kuttner',
+      read: false
+    },
+    {
+      title: 'The Wind in the Willows',
+      genre: 'Fantasy',
+      author: 'Kenneth Grahame',
+      read: false
+    },
+    {
+      title: 'Life On The Mississippi',
+      genre: 'History',
+      author: 'Mark Twain',
+      read: false
+    },
+    {
+      title: 'Childhood',
+      genre: 'Biography',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    }];
+  bookRouter.route('/').get((req, res) => res.render('bookList', { nav: navigation, title: 'Library', books }));
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;// Object DeStructuring
+    res.render('bookSingle', { nav: navigation, title: 'Library', book: books[id] });
+  });
+  return bookRouter;
+}
+
+module.exports = router;
