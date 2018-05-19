ff7992ac46c45971373d9a4346b89a14c89d1f06
SageGandhi <prajit.ju@gmail.com>
2018-05-19 13:00:24 +0530

    Db Complete

--- JavaScript/Js.01/05.NodeWebExpress/Chap06.DataBase.NoSql/App01.js
+++ JavaScript/Js.01/05.NodeWebExpress/Chap06.DataBase.NoSql/App01.js
@@ -1,22 +1,17 @@
-// Keep All Require At The Top
 const express = require('express');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
 const debug = require('debug')('Application');
-const sql = require('mssql');
-// Keep All Variable After Require
+
 const app = express();
 const port = process.env.Port || 3000;
 const nav = [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }];
-const config = { "user": "****ChangeIt****", "password": "****ChangeIt****", "server": "****ChangeIt****", "database": "****ChangeIt****", "options": { "encrypt": true } };
-const bookRouter = require('./src/routes/bookRouter')(nav);//Created Wrapped Function
+const bookRouter = require('./src/routes/bookRouter')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+
 // MiddleWare Setup
-sql.connect(config).catch(err => debug(err));//Connect With SqlServer
 app.use(morgan('tiny'));
-app.use((req, res, next) => {//Check Usage In BooksRouter
-    debug('Middleware Intercepted'); next();
-});
 app.use(express.static(path.join(__dirname, 'public')));// Serving Static Files
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
@@ -24,7 +19,9 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'di
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 // Start Operation
 app.get('/', (req, res) => res.render('index', { nav, title: 'Library' }));
 // Listening To Port: node
 app.listen(port, () => debug(`Server Started @ ${chalk.green(port)}`));
+//$env:DEBUG = "*,-not_this"
--- JavaScript/Js.01/05.NodeWebExpress/Chap06.DataBase.NoSql/src/routes/bookRouter.js
+++ JavaScript/Js.01/05.NodeWebExpress/Chap06.DataBase.NoSql/src/routes/bookRouter.js
@@ -1,87 +1,52 @@
 const express = require('express');
-const sql = require('mssql');// Same Instance As In App01.js
-const debug = require('debug')('Application:BookRouter');
+const { MongoClient, ObjectID } = require('mongodb');
+const debug = require('debug')('BookRotes');
+
 // Wrap Inside A Function To Pass Config Fo Navigation Object
 const bookRouter = express.Router();
 function router(navigation) {
   const books = [
     {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Misérables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false
-    },
-    {
       title: 'A Journey into the Center of the Earth',
       genre: 'Science Fiction',
       author: 'Jules Verne',
       read: false
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false
-    },
-    {
-      title: 'The Wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false
-    },
-    {
-      title: 'Life On The Mississippi',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: false
-    },
-    {
-      title: 'Childhood',
-      genre: 'Biography',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
     }];
+  const url = 'mongodb://Mongo:MzfD7FPf4GOnttet@mongows-shard-00-00-1ae3q.mongodb.net:27017/libraryApp?ssl=true&replicaSet=MongoWs-shard-0&authSource=admin&retryWrites=true';
+  const dbName = 'libraryApp';
   bookRouter.route('/').get((req, res) => {
-    (async function query() {
-      const sqlRequest = new sql.Request();
 
-      const { recordset } = await sqlRequest.query('select * from books');// AsyncWait
-      debug('All Book:', recordset);// If Success Return Database Books
-
-      res.render('bookList', { nav: navigation, title: 'Library', books: recordset });
-      if (!recordset) {
+    (async function mongo() {
+      const client = await MongoClient.connect(url, { useNewUrlParser: true });
+      try {
+        debug('Connected Correctly To Server');
+        const db = client.db(dbName);
+        const collection = await db.collection('books');
+        const booksDb = await collection.find().toArray();
+        res.render('bookList', { nav: navigation, title: 'Library', books: booksDb });
+      } catch (error) {
         res.render('bookList', { nav: navigation, title: 'Library', books });
       }
-    }());// Iife
-  });
-  bookRouter.route('/:id').all((req, res, next) => {
-    (async function query() {
-      const { id } = req.params;// Object De-Structuring
-      const sqlRequest = new sql.Request();
-      const { recordset } = await sqlRequest
-        .input('id', sql.Int, id)
-        .query('select * from books where id = @id');// AsyncWait
-      debug('Book:', recordset);
-      [req.book] = recordset;// Array De-Structuring
+      client.close();
     }());
-    next();
-  }).get((req, res) => {
-    res.render('bookSingle', { nav: navigation, title: 'Library', book: req.book });
-    if (!req.book) {
-      res.render('bookSingle', { nav: navigation, title: 'Library', book: books[req.params.id] });
-    }
+  });
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;// Object DeStructuring
+    (async function mongo() {
+      const client = await MongoClient.connect(url, { useNewUrlParser: true });
+      try {
+        debug('Connected Correctly To Server');
+        const db = client.db(dbName);
+        const collection = await db.collection('books');
+        // Requires ObjectID
+        const booksDb = await collection.findOne({ _id: new ObjectID(id) });
+        debug(`From Db:${booksDb}`);
+        res.render('bookSingle', { nav: navigation, title: 'Library', book: booksDb });
+      } catch (error) {
+        res.render('bookSingle', { nav: navigation, title: 'Library', book: books[0] });
+      }
+      client.close();
+    }());
   });
   return bookRouter;
 }
