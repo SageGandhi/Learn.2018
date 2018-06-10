8319a815f7bfc60a8931a14dcced993e48572de6
SageGandhi <prajit.ju@gmail.com>
2018-06-10 23:40:38 +0530

    DataBinding&Pipes

--- Angular/01.AcmeProductManager/src/app/app.component.ts
+++ Angular/01.AcmeProductManager/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core'
 @Component({
   selector:"pm-root",
-  template:`<div><h4>{{pageTitle}} From Scratch.</h4></div>`
+  template:`<div><h4>{{pageTitle}} From Scratch.</h4><pm-products></pm-products></div>`
 })
 export class AppComponent{
   pageTitle:string = 'Prajit Learns Angular';
--- Angular/01.AcmeProductManager/src/app/app.module.ts
+++ Angular/01.AcmeProductManager/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 import {AppComponent} from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+
 /**
  * BrowserModule:imports Says This Component Is Available To All Other Component Of AppModule
  * AppComponent:declarations Says This Component Is Part Of AppModule
  * AppComponent:bootstrap StartUp Compoment Which Contains Selector Used In index.html
  */
 @NgModule({
-  imports:[BrowserModule],
+  imports:[BrowserModule,FormsModule],
   bootstrap:[AppComponent],
-  declarations:[AppComponent]
+  declarations:[AppComponent,ProductListComponent]
 })
 export class AppModule{}
